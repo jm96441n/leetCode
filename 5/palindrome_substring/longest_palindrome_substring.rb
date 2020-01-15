af7d6d12cda0450a0d0a39b7e4b8860624de46f6
jm96441n <jmaguire@mdsol.com>
2020-01-15 18:27:23 -0500

    longest palindrome

--- 5/palindrome_substring/longest_palindrome_substring.rb
+++ 5/palindrome_substring/longest_palindrome_substring.rb
@@ -5,11 +5,31 @@ extend Test::Unit::Assertions
 def longest_palindrome(s)
   is_palindrome = ->(string) { string == string.reverse }
 
+  return s if is_palindrome.call(s)
+
+  maxes = []
+  arr = s.split('').join('|').split('').push('|').unshift('|')
+  longest = ''
+  i = 0
+
+  while i < arr.length
+    j = 1
+    j += 1 while i - j >= 0 && arr[i + j] == arr[i - j]
+    j -= 1
+    maxes << arr[(i - j)..(i + j)].length / 2
+
+    i += 1
+  end
+  max = maxes.max
+  longest = s[((maxes.index(max) - max) / 2)..(((maxes.index(max) + max) / 2) - 1)]
+  longest
 end
 
 test_cases = {
-  'babad' => 'aba',
-  'cbbd' => 'bb'
+  'abad' => 'aba',
+  'cbbd' => 'bb',
+  'a' => 'a',
+  'ac' => 'a'
 }
 
 test_cases.each do |k, v|
